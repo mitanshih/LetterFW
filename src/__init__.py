7efcd3402edb65140e8153127509133b863fc766
mitanshih <milkteashih@gmail.com>
2025-02-06 16:24:40 +0800

    Add the script path into the system.

--- src/__init__.py
+++ src/__init__.py
@@ -6,6 +6,8 @@ Created on 2025-01-19 01:31:32
 '''
 
 #%%    Packages
+import sys
+
 from pathlib import Path
 
 #%%    Variable
@@ -14,3 +16,11 @@ files = Path(__file__).parent.glob('*.py')
 
 # Append the module names to `__all__` except __init__.py.
 __all__ = [f.stem for f in files if f.is_file() and f.name != '__init__.py']
+
+# Add 'src' folder to sys.path that Python can find the modules.
+script_path: str = (Path(__file__).resolve().parents[1] / "src").as_posix()
+if script_path not in sys.path:
+    sys.path.append(script_path)
+
+    # Using insert(0, path) to make sure the path has the highest priority.
+    #sys.path.insert(0, script_path)
